8b6d9444f4500f46ea12ffaca7127794e6a48a2f
MD:Rubrl RM <inforubel251@gmail.com>
2023-07-23 17:47:35 +0600

    add to the admission api create

--- index.js
+++ index.js
@@ -8,6 +8,71 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oikc1wt.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const userCollection = client.db('CollegeBooking').collection('Users')
+        const collegesCollection = client.db('CollegeBooking').collection('Colleges')
+        const admissionsCollection = client.db('CollegeBooking').collection('admissions')
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const query = { email: user.email }
+            const exitingUser = await userCollection.findOne(query)
+            if (exitingUser) {
+                return res.send({ message: "Is User Already Exist" })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result)
+        })
+
+
+        app.get('/colleges', async (req, res) => {
+            const result = await collegesCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        app.post('/admissions', async (req, res) => {
+            const admission = req.body
+            const result = await admissionsCollection.insertOne(admission);
+            res.send(result)
+        })
+
+        app.get('/my_admissions', async (req, res) => {
+            const result = await admissionsCollection.find().toArray()
+            res.send(result)
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('College is open')
 })
